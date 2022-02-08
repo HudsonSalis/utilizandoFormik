f6b16bfd29b9298c523d72530834e4ae32c82d7b
hudsonsalis <hudsonsalis50@yahoo.com>
2022-02-08 17:20:33 -0300

    F

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './components/formulario';
+
+function App() {
+
+
+  return (
+    <>
+      <Formulario />
+    </>
+  )
+}
+
+export default App;
